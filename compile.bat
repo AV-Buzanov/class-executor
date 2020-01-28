b34ecae94a4ef1108e06c476055fec6aeb375d67
buzanov <av.buzanov@gmail.com>
2020-01-28 13:10:23 +0300

    Рабочий вариант компиляции и запуска из файла.

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 @echo off
-set x32="C:\Program Files\Java\jdk1.8.0_192\bin\javac.exe"
+set javapath="C:\Program Files\Java\jdk1.8.0_192\bin\javac.exe"
 echo Start ...................... Ok
 
-%x32% -cp D:\com\com D:\com\code\CallableClass.java
+%javapath% %filepath%
  
 echo Complete .................... Ok
 pause
--- src/main/java/org/example/controller/ClassExecutorController.java
+++ src/main/java/org/example/controller/ClassExecutorController.java
@@ -11,14 +11,16 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.zeroturnaround.exec.ProcessExecutor;
 import sun.misc.Unsafe;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.logging.ConsoleHandler;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,9 +32,28 @@ import java.util.stream.Stream;
 @RequestMapping(value = "/file")
 public class ClassExecutorController {
 
+    private String compileFile(File file) throws Exception {
+//        File file = new File("D:\\com\\code\\CallableClass.java");
+
+        List<String> commands = Arrays.asList("cmd", "/c", "set filepath=\""+file.getPath()+"\" && ",
+                "C:\\Users\\buzanov\\IdeaProjects\\ClassExecutor\\compile.bat");
+// or Arrays.asList("/bin/sh", "yourScript.sh");
+//        List<String> commands = Arrays.asList("cmd", "/c", "C:\\Program Files\\Java\\jdk1.8.0_192\\bin\\javac.exe"
+//                        +file.getPath()+"echo ok");
+
+        String output = new ProcessExecutor()
+                .command(commands)
+                .readOutput(true)
+                .execute()
+                .outputUTF8();
+//        int i = Runtime.getRuntime().exec("cmd java -jar D:\\com\\ClassExecutor-1.0-SNAPSHOT.jar").exitValue();
+
+        return output;
+    }
+
+
     @PostMapping(value = "/compile")
     public String compile(MultipartHttpServletRequest data) throws Exception {
-        data.getFileNames().forEachRemaining(System.out::println);
         MultipartFile mfile = data.getFile("file");
         if (mfile == null)
             throw new NullPointerException("Файл пустой");
@@ -44,17 +65,45 @@ public class ClassExecutorController {
                 new BufferedOutputStream(new FileOutputStream(file));
         stream.write(bytes);
         stream.close();
-        List<String> commands = Arrays.asList("cmd", "/c", "D:\\com\\compile.bat");
-// or Arrays.asList("/bin/sh", "yourScript.sh");
 
-        String output = new ProcessExecutor()
-                .command(commands)
-                .readOutput(true)
-                .execute()
-                .outputString();
+        String output = compileFile(file);
+        Thread.sleep(1000);
+        String pathss = mfile.getOriginalFilename().replace(".java",".class");
+        System.out.println(pathss);
+
+        File classfile = new File("D:\\com\\" + pathss);
+
+
+        if (!classfile.exists())
+            throw new NullPointerException("Файл пустой");
+
+//        FileInputStream inputStream = new FileInputStream(classfile);
+        byte[] filebytes = Files.readAllBytes(classfile.toPath());
+//        inputStream.read(filebytes);
+//        Thread.sleep(500);
+//        inputStream.close();
+
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        final Field theUnsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafeField.setAccessible(true);
+        final Unsafe unsafe = (Unsafe) theUnsafeField.get(null);
+        final Class<?> myCustomInvocator = unsafe.defineClass(null, filebytes, 0,
+                filebytes.length, classLoader, null);
+
+        ByteArrayOutputStream baOut = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(baOut);
+        System.setOut(out);
+        System.setErr(out);
+
+        Reflection.runMainInSameVM(myCustomInvocator, null);
+        String s = new String(baOut.toByteArray());
+        System.setOut(System.out);
+        System.setErr(System.err);
+
 //        int i = Runtime.getRuntime().exec("cmd java -jar D:\\com\\ClassExecutor-1.0-SNAPSHOT.jar").exitValue();
 
-        return "ok"+ output;
+        return "ok"+ output+s;
     }
 
     @PostMapping(value = "/loadCallable")
@@ -120,7 +169,6 @@ public class ClassExecutorController {
 
     @PostMapping(value = "/loadAndSave")
     public ResponseEntity<String> loadAndSave(MultipartHttpServletRequest data) throws Exception {
-        data.getFileNames().forEachRemaining(System.out::println);
         MultipartFile mfile = data.getFile("file");
         if (mfile == null)
             throw new NullPointerException("Файл пустой");
